64aafe890b0e1f22727316065bd3d1d4d0546c33
Alexandra Long <ael2203@columbia.com>
2023-04-29 11:17:18 -0400

    Code elements for the website

---
+++ static/home.js
@@ -0,0 +1,13 @@
+// function get_photo() {
+//     $.ajax({
+//         type: "POST",
+//         url: "get_photo",
+//         dataType: "json",
+//         contentType:
+//     })
+// }
+// $(document).ready(function() {
+//     $("#Submit").click(function() {
+//         get_photo()
+//     })
+// })
